c4283b3ba8fc147304f681865f446a8b7e94120c
PressStart10 <iltsn29@sudburystudents.org>
2020-11-26 16:31:28 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("hello, i will be your virtual pet, put \"start\" to begin");
+    string stat = Console.ReadLine();
+    if(stat == "start"){
+      
+    }
+	}
+}
